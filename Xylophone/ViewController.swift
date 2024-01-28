405b3c568e13b3b0d2307a32ce9a73562a7ad2a5
Riyan Pahuja <riyanpahuja@Riyans-MacBook-Pro.local>
2024-01-28 14:54:49 +1100

    playing functionality added;

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,14 +7,28 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+        print(sender.currentTitle ?? "C")
+        playSound(soundLabel: sender.currentTitle ??
+        "C")
+    }
+    
+    func playSound(soundLabel: String){
+        let url = Bundle.main.url(forResource: soundLabel, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+    }
+    
     
 
 }
